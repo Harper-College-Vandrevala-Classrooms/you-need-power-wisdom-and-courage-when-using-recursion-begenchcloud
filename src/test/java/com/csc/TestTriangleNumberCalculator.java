4fa2a9357c441cfc8b83360b3d2df9a23edfe113
Ben <b_abdyvaidov@mail.harpercollege.edu>
2024-10-20 20:49:06 -0500

    finished

--- src/test/java/com/csc/TestTriangleNumberCalculator.java
+++ src/test/java/com/csc/TestTriangleNumberCalculator.java
@@ -1,6 +1,8 @@
 package com.csc;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 public class TestTriangleNumberCalculator {
 
@@ -11,5 +13,29 @@ public class TestTriangleNumberCalculator {
     calculator = new TriangleNumberCalculator();
   }
 
-  // Your tests go here
+  @Test
+  void testValueMethod() {
+    
+    assertEquals(1, calculator.value(1));
+    assertEquals(3, calculator.value(2));
+    assertEquals(6, calculator.value(3));
+    assertEquals(10, calculator.value(4));
+    assertEquals(15, calculator.value(5));
+  }
+
+  @Test
+  void testAddMethod() {
+
+    assertEquals(2, calculator.add(1, 1));
+    assertEquals(9, calculator.add(2, 3));
+    assertEquals(13, calculator.add(4, 2));
+  }
+
+  @Test
+  void testSubtractMethod() {
+
+    assertEquals(0, calculator.subtract(1, 1));
+    assertEquals(-3, calculator.subtract(2, 3));
+    assertEquals(7, calculator.subtract(4, 2));
+  }
 }
